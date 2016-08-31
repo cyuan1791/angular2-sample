db71951f1fa4f9de81b818117a859685f4aeadd2
cmsnow <cyuan123@live.com>
2016-08-30 21:29:09 -0700

    Clean up code

--- angular2-samples/app/lessons/start/9-auth-firebase/shared/auth.service.ts
+++ angular2-samples/app/lessons/start/9-auth-firebase/shared/auth.service.ts
@@ -6,11 +6,11 @@ declare var firebase: any;
 export class AuthService {
     sigupUser(user: User) {
         console.log(user);
-        firebase.auth().createUserWithEmailAndPassword(user.email, user.password).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // ...
-});
+        firebase.auth().createUserWithEmailAndPassword(user.email, user.password).catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // ...
+        });
     }
 }
